d92527056165b65cbbe9a1250daa72f5b9fb12e2
DoNalD-A <thdlstjrj@naver.com>
2025-06-16 11:38:46 +0900

    feature: 예외 응답 추가

--- src/main/java/com/parkmate/userservice/common/response/ResponseStatus.java
+++ src/main/java/com/parkmate/userservice/common/response/ResponseStatus.java
@@ -16,13 +16,14 @@ public enum ResponseStatus {
     /**
      * 4xx: 클라이언트 오류
      **/
-    FAILED_TO_FIND_USER(HttpStatus.NOT_FOUND, false, 40401, "해당 유저를 찾을 수 없습니다."),
+    FAILED_TO_FIND_USER(HttpStatus.NOT_FOUND, false, 404, "해당 유저를 찾을 수 없습니다."),
+    ALREADY_EXIST_FAVORITE(HttpStatus.CONFLICT, false, 409, "이미 즐겨찾기가 존재합니다."),
+    ALREADY_EXIST_USER(HttpStatus.CONFLICT, false, 409, "이미 유저가 존재합니다."),
 
     /**
      * 5xx: 서버 오류
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, 500, "서버 내부 오류가 발생했습니다."),
-    ;
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, 500, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final boolean isSuccess;
